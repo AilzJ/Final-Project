ee37f59291a493b9f6557056cbe298c31f2048ca
AilzJ <125991229+AilzJ@users.noreply.github.com>
2023-04-10 18:29:24 +0200

    Delete current directory to rectify errors in previous upload

--- Project Work/index.js
+++
@@ -1,9 +0,0 @@
-var confettiButton = document.getElementById("confetti-button-header");
-
-confettiButton.addEventListener("click", clickForConfetti);
-
-function clickForConfetti() {
-  const jsConfetti = new JSConfetti();
-
-  jsConfetti.addConfetti();
-}
